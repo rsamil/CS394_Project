53f18d4a15fc6713882b2d2626add2eae5963be5
Eren Darak <eren.darak@ozu.edu.tr>
2025-01-08 23:44:05 +0300

    BugFix for GameListViewModel (getting multiple pages)

--- app/src/main/java/com/example/gamerecord_app/GameListViewModel.kt
+++ app/src/main/java/com/example/gamerecord_app/GameListViewModel.kt
@@ -12,28 +12,41 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class GameListViewModel : ViewModel() {
+    private var currentPage = 1
+    private val maxPages = 5
+    private val pageSize = 40
+    private val allGames = mutableListOf<Game>()
+
     private val _gameList = MutableLiveData<List<Game>>()
     val gameList: LiveData<List<Game>> get() = _gameList
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
-    private val apiService: RAWGApiService = ApiClient.apiService
-
     init {
         fetchGames()
     }
 
     fun fetchGames() {
+        if (currentPage > maxPages) return
+
         _isLoading.value = true
-        apiService.getGames().enqueue(object : Callback<GameResponse> {
+        val apiService = ApiClient.retrofit.create(RAWGApiService::class.java)
+
+        apiService.getGames(currentPage, pageSize).enqueue(object : Callback<GameResponse> {
             override fun onResponse(call: Call<GameResponse>, response: Response<GameResponse>) {
                 _isLoading.value = false
                 if (response.isSuccessful) {
-                    _gameList.value = response.body()?.results ?: emptyList()
+                    val newGames = response.body()?.results ?: emptyList()
+                    allGames.addAll(newGames)
+                    _gameList.value = allGames
+
+                    currentPage++
+                    if (currentPage <= maxPages) {
+                        fetchGames()
+                    }
                 }
             }
-
             override fun onFailure(call: Call<GameResponse>, t: Throwable) {
                 _isLoading.value = false
             }
